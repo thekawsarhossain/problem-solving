704122e82c142d558d66edcfd2d05502940fa981
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-22 22:46:21 +0600

    factorials added

---
+++ factorials.js
@@ -0,0 +1,10 @@
+function factorials(number) {
+  let factorial = 1;
+
+  for (let i = 1; i <= number; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
+}
+
+console.log(factorials(8));
